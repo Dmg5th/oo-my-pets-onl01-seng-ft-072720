3ce0d8aec7d9f4787787b45f4f46c6109106a0ff
Daniel Gould <dmgould310@gmail.com>
2020-08-11 18:32:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,13 +3,20 @@ require 'pry'
 class Owner
     attr_reader :name, :species 
     
+    @@all = []
+    
     def initialize (name_arg)
       @name=name_arg
-      @species="human"     
+      @species="human" 
+      @@all << self 
     end
     
     def say_species
      "I am a #{@species}."  
+    end 
+    
+    def self.all 
+      @@all   
     end 
     
 
