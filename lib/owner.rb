5e326067f844657ff81f52c090ef700cc38ea5d7
Daniel Gould <dmgould310@gmail.com>
2020-08-11 18:18:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,13 +3,12 @@ require 'pry'
 class Owner
   # code goes here
 
-  attr_reader :name, :species, :cat  
+  attr_reader :name
     @@all =[]
 
 
   def initialize (name_arg)
     @name=name_arg
-    @species="human"
     @@all << self
 
   end
