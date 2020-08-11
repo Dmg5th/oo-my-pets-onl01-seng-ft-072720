07ea9d0a3673626671cb13020141bfd0ff129dda
Daniel Gould <dmgould310@gmail.com>
2020-08-11 18:36:30 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat 
     attr_accessor :owner 
-    attr_reader :name 
+    attr_reader :name, :mood 
   
     def initialize(name_arg,owner_arg)
     @name = name_arg 
     @owner = owner_arg
+    @mood = "nervous"
     end 
   
   
