917e74d6558c75c40deac226fb7248a193f8bb8f
Daniel Gould <dmgould310@gmail.com>
2020-08-11 18:40:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,18 @@ class Dog
     attr_accessor :owner, :mood 
     attr_reader :name 
   
+    @@all = []
+    
     def initialize(name_arg,owner_arg)
       @name = name_arg 
       @owner = owner_arg
       @mood = "nervous"
+      @@all << self 
     end 
   
+    def self.all 
+      @@all   
+    end 
   
   
 end 
